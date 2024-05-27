4fadc44d09358657499fb27809900d5b87dab6b3
Rupe88 <chyrupesh828@gmail.com>
2024-05-27 21:08:32 +0545

    some changes in models

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,40 +1,44 @@
 // User.ts
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-import { Admin } from './Admin';
-import { Teacher } from './Teacher';
-import { Student } from './Student';
-
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Admin } from "./Admin";
+import { Teacher } from "./Teacher";
+import { Student } from "./Student";
+import { Transform } from "class-transformer";
+import { IsEmail, MinLength } from "class-validator";
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
+  @MinLength(4)
   username: string;
 
   @Column()
-  email:string;
+  @IsEmail()
+  email: string;
 
   @Column()
+  @MinLength(6)
   password: string;
 
   @Column({ nullable: true }) // Allow null to indicate no avatar uploaded
   avatar: string;
 
   @Column({
-    type: 'enum',
-    enum: ['admin', 'teacher', 'student'],
-    default: 'student', // Default role is student
+    type: "enum",
+    enum: ["admin", "teacher", "student"],
+    default: "student", // Default role is student
   })
+  @Transform(({ value }) => value.trim().toLowerCase())
   role: string;
-  lowercase:true;
 
-  @OneToMany(() => Admin, admin => admin.user)
+  @OneToMany(() => Admin, (admin) => admin.user)
   admins: Admin[];
 
-  @OneToMany(() => Teacher, teacher => teacher.user)
+  @OneToMany(() => Teacher, (teacher) => teacher.user)
   teachers: Teacher[];
 
-  @OneToMany(() => Student, student => student.user)
+  @OneToMany(() => Student, (student) => student.user)
   students: Student[];
 }
